1cac25a7adb035c4ed276e927ce5695b9320d8b8
luciakusumaw <itsmelucia27@gmail.com>
2025-11-27 07:14:32 +0700

    Question 1, no 5

--- Cinema13.java
+++ Cinema13.java
@@ -16,10 +16,9 @@ public class Cinema13 {
         System.out.printf("%s \t %s\n", audience [3][0], audience[3][1] );
 
         System.out.println(audience.length);
-        System.out.println(audience[0].length);
-        System.out.println(audience[1].length);
-        System.out.println(audience[2].length);
-        System.out.println(audience[3].length);
-        
+        for (int i=0; i<audience.length; i++) {
+            System.out.println("Length of row " + (i+1)+ ": " + audience[i].length);
+        }
+
     }
 }
